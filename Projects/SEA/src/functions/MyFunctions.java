b8cba0d4d319904ccd9784227af1263f0145a074
klatysheva <latysheva.ks@yandex.ru>
2021-04-13 22:29:47 +0300

    Test Project initial commit

---
+++ Projects/SEA/src/functions/MyFunctions.java
@@ -0,0 +1,29 @@
+package functions;
+
+public class MyFunctions {
+    public static void main(String[] args) {
+        isEven(6);
+    }
+
+    static boolean isEven (int num) {
+        if (num == 0) {
+            System.out.println("It's 0!");
+            return false;
+        }
+        else if (num%2==0) {
+            System.out.println("It's an even number!");
+            return true;
+        }
+        else if (num%2!=0){
+            System.out.println("It's an odd number!");
+            return false;
+        }
+        else {
+            System.out.println("Something went wrong");
+            return false;
+        }
+    }
+    static double hyperbola(int a) {
+        return 1 / (double)a;
+    }
+}
