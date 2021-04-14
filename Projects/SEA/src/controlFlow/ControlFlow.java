71e1aed196d86be3246bac5ef8273864875a951a
klatysheva <latysheva.ks@yandex.ru>
2021-04-14 19:42:39 +0300

    Refactoring + Control Flow

--- Projects/SEA/src/controlFlow/ControlFlow.java
+++ Projects/SEA/src/controlFlow/ControlFlow.java
@@ -9,6 +9,12 @@ public class ControlFlow {
     static void multiplicationTable () {
         for (int i = 1; i<11; i++) {
             for (int j = 1; j<11; j ++) {
+                if (i * j < 10) {
+                    System.out.print("  ");
+                }
+                else if (i * j < 100) {
+                    System.out.print(" ");
+                }
                 System.out.print(i * j + " ");
             }
             System.out.println();
