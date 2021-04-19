41f47450fb4f45b804d826068adfd302c4c5f9b9
klatysheva <latysheva.ks@yandex.ru>
2021-04-19 13:24:21 +0300

    Quest 1593: java.lang.Object

---
+++ Projects/SEA/src/java_lang_Object/Animal.java
@@ -0,0 +1,17 @@
+package java_lang_Object;
+
+public class Animal {
+    private String species;
+
+    public Animal(String species) {
+        this.species = species;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+}
